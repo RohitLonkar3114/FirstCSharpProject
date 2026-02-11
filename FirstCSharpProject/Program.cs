87b003428977e9a77ce2d95d5a196648668565b1
RohitLonkar3114 <rohitlonkar3114@gmail.com>
2026-02-11 18:20:05 +0530

    Add project files.

---
+++ FirstCSharpProject/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstCSharpProject
+{
+    internal class Program
+    {
+        public void Add()
+        {
+            int a, b, c;
+            a = 20;
+            b = 10;
+            c = a + b;
+            Console.WriteLine("Addition = "+c);
+        }
+        public void Mul()
+        {
+            int a, b, c;
+            a = 20;
+            b = 10;
+            c = a * b;
+            Console.WriteLine("Multiplication = "+c);
+        }
+        static void Main(string[] args)
+        {
+            Program p=new Program();
+            p.Add();
+            p.Mul();
+            Console.ReadLine();
+        }
+    }
+}
